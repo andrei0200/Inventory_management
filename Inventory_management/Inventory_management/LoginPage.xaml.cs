95d9810533dedf488e3875ea9c8b9ac97052990c
Iancu Andrei <79975904+andrei0200@users.noreply.github.com>
2021-11-14 20:35:01 +0200

    Pagina login + register

---
+++ Inventory_management/Inventory_management/LoginPage.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Inventory_management
+{
+    /// <summary>
+    /// Interaction logic for LoginPage.xaml
+    /// </summary>
+    public partial class LoginPage : Window
+    {
+        static String connectionString = "Server=.;Database=Inventory_Management;Trusted_Connection=true";
+        SqlConnection connection = new SqlConnection(connectionString);
+        DataSet set = new DataSet();
+        SqlDataAdapter adapter = new SqlDataAdapter();
+
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            String pass = password_text.Text;
+            String username = username_text.Text;
+
+            if (pass == "" || username == "")
+            {
+                MessageBox.Show("Nu ati introdus credentialele necesare!","EROARE", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+            if (pass == "pass" && username == "user")
+            {
+                MainWindow mainW = new MainWindow();
+                mainW.Show();
+                this.Close();
+                MessageBox.Show("Bine ai venit " + username, ":)");
+            }
+            else
+            {
+                MessageBox.Show("Nume sau parola incorecta!","ERAORE", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            //connection.Open();
+            //var cmd = connection.CreateCommand();
+            //cmd.CommandType = System.Data.CommandType.Text;
+            //cmd.CommandText = "select * from Utilizatori where Nume_Utilizator='" + username + "'and Parola='" + pass + "'";
+
+            //using (var rezultat = cmd.ExecuteReader())
+            //{
+            //    if (rezultat.Read())
+            //    {
+            //        MessageBox.Show("Bine ai venit " + username);
+            //    }
+            //    else
+            //    {
+            //        MessageBox.Show("Nume sau parola incorecta!");
+            //    }
+            //}
+            //connection.Close();
+        }
+
+        private void Label_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            RegisterPage RP = new RegisterPage();
+            RP.Show();
+            this.Close();
+        }
+    }
+}
---
+++ Inventory_management/Inventory_management/RegisterPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Inventory_management
+{
+    /// <summary>
+    /// Interaction logic for RegisterPage.xaml
+    /// </summary>
+    public partial class RegisterPage : Window
+    {
+        public RegisterPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Label_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            LoginPage login = new LoginPage();
+            login.Show();
+            this.Close();
+        }
+    }
+}
